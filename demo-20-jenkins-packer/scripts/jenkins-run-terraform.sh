5fa1b408c1ed34dfb69fb35b1235a064331db02b
nguyenbuitk <buinguyen23112k@gmail.com>
2023-06-24 14:38:35 +0000

    fix script

--- demo-20-jenkins-packer/scripts/jenkins-run-terraform.sh
+++ demo-20-jenkins-packer/scripts/jenkins-run-terraform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 AWS_REGION="eu-west-1"
-cd jenkins-packer-demo
+cd demo-20-jenkins-packer
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
